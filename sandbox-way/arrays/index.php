167de0bafd7a0697de50cf290f018a796432af99
angelamaunz <alynnem0z@gmail.com>
2015-07-26 16:16:16 -0600

    arrays associate

--- sandbox-way/arrays/index.php
+++ sandbox-way/arrays/index.php
@@ -5,9 +5,16 @@
 /*$month = array('Jan', 'Feb', 'Mar', 'Apr', 'May');
 echo $month[4];*/
 
-$tuts_sites = array('nettuts+', 'psdtuts', 'webdesigntuts', 'wptuts', 'anothertuts');
-print_r($tuts_sites);
+// $tuts_sites = array('nettuts+', 'psdtuts', 'webdesigntuts', 'wptuts', 'anothertuts');
+// print_r($tuts_sites);
 
+//$tuts_sites = ['nettuts+', 'psdtuts', 'webdesigntuts', 'wptuts', 'anothertuts'];
+$tuts_sites = array(
+	'nettuts' => 'http://net.tutsplus.com',
+	'psdtuts' => 'http://psd.tutsplus.com',
+	'webdesigntuts' => 'http://webdesign.tutsplus.com',
+	'wptuts' => 'http://net.wp.tutsplus.com',
+);
 
 ?>
 
