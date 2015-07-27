71194c2a9683fc964508401931c15dd0a9c61716
angelamaunz <alynnem0z@gmail.com>
2015-07-27 15:23:58 -0600

    conditionals

--- sandbox-way/conditionals/index.php
+++ sandbox-way/conditionals/index.php
@@ -1,72 +1,22 @@
 <?php
 
-$month = 'June';
+$month = 2; // set month = Jan
+// $month == 'January'; // does month = 'Jan'?
 
-
-/*
- * If Statements
- */
-
-// if($month == 'January') {
-// 	echo 'It is Jan!';
-// } elseif($month == 'February') {
-// 	echo 'It is Feb!';
-// } elseif($month == 'March') {
-
-// } else {
-// 	echo 'Not the right month!';
-// }
-
-
-/*
- * Switch Statements
- */
-
-// switch($month) {
-// 	case 'January':
-// 		echo 'It is Jan!';
-// 		break;
-
-// 	case 'February':
-// 		echo 'It is Feb!';
-// 		break;
-
-// 	case 'March':
-// 		echo 'It is March!';
-// 		break;		
-
-// 	default:
-// 		echo 'Not the right month!';
-// }
-
-
-
-
-/*
- * Lookups
- */
-
-// $months = array(
-// 	'January'   => 'It is Jan',
-// 	'February'  => 'It is February',
-// 	'March' 	=> 'It is March'
-// );
-
-// echo isset($months[$month]) ? $months[$month] : 'Not the right month!';
-// echo $months['January'];
-
-
-
-/*
- * and an or (or && and ||)
- */
-
-if ( $month == 'May' or $month == 'June' ) {
-	echo 'Is May or June';
+if($month >= 0) {
+	echo 'It does!';// do something
 } else {
-	echo 'It is not one of those.';
+	echo 'Nope';
 }
 
+$month = 'January';
 
+switch($month) {
+	case 'January';
+		echo 'It is Jan!';
+		break;
 
-
+	case 'February';
+		echo 'It is Feb!';
+		break;
+}
