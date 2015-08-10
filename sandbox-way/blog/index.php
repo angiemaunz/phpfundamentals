cb98c3cd489e614932cdea18eefd232d8c069a54
angelamaunz <alynnem0z@gmail.com>
2015-08-10 11:37:40 -0600

    blog end of 1 and some of 2

---
+++ sandbox-way/blog/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 8/9/15
+ * Time: 7:31 AM
+ */
---
+++ sandbox-way/blog/views/index.view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 8/9/15
+ * Time: 7:32 AM
+ */
